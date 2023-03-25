246c75e9535ac408f3284c3d2f6ca22b7cb9b094
asTsiry <astsiry60@gmail.com>
2023-03-25 15:18:09 +0300

    Page profile

--- src/routes/profils.js
+++ src/routes/profils.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async function(req, res, next) {
-  res.render('profils');
+  res.render('profile/profils');
 });
 
 module.exports = router;
